c6e16769099edfdba2b24888a2c1ea37efc32428
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-25 00:06:36 +0000

    task 0 & 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-"""
+""" Task 1"""
 import csv
 import math
 from typing import List, Tuple
@@ -10,6 +9,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -32,7 +32,7 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert isinstance(page, int)
-        assert isinstance(page_size, int) 
+        assert isinstance(page_size, int)
         page > 0
         page_size > 0
         start_index, end_index = index_range(page, page_size)
