49f7d71854370cb48d77dcf71195cad882799f7c
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-26 23:44:28 +0000

    basic caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,11 +7,11 @@ class BasicCache(BaseCaching):
     ''' Basic cache class '''
 
     def put(self, key, item):
-        ''' Assign the value to the key in the dictionary if key is not None '''
+        ''' Assign the value to the key in dictionary if key is not None '''
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        ''' If key is None or if the key doesn't exist in cache_data, return None'''
+        ''' If key is None or if key doesn't exist in cache_data return None'''
         return self.cache_data.get(key, None)
