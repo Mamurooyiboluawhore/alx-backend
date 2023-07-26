5a95d7d03119c2f5ec1a7d799f04fe3252cf9b02
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-26 23:34:18 +0000

    updated the condition

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,16 +5,11 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     ''' Basic cache class '''
-    def __init__(self):
-        ''' Create an empty dictionary as an instance'''
-        self.cache_data = {}
-
     def put(self, key, item):
         # Assign the value to the key in the dictionary if key is not None
-        if key:
-            self.cache_data[key] = item
-        else:
-            pass
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         # If key is None or if the key doesn't exist in cache_data, return None
