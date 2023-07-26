4e5d21d64e58c149b7b25a36360dc77f6bb5b2c0
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-26 01:09:05 +0000

    Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,14 +4,18 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    ''' Basic cache class '''
     def __init__(self):
+        # Create an empty dictionary as an instance
         self.cache_data = {}
 
     def put(self, key, item):
+        # Assign the value to the key in the dictionary if key is not None
         if key:
             self.cache_data[key] = item
 
     def get(self, key):
+        # If key is None or if the key doesn't exist in cache_data, return None
         if key is None:
             return None
         return self.cache_data.get(key, None)
