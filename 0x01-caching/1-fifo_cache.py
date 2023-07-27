92f4d405188831161f6f5b4c6a5d4bd985a2ff47
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-27 00:24:59 +0000

    fifo caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' FIFO caching'''
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    ''' Fifo cache'''
+    def __init__(self):
+        ''' Call the __init__ method of the parent class using super()'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        ''' add an item to the cache'''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest_key = self.cache_data.popitem(False)
+            print('DISCARD:', oldest_key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+        return self.cache_data.get(key)
